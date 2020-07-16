8d3447368f1add81c5e6d5d446418d8fadca6baf
Sandro <sandroderios@hotmail.com>
2020-07-16 13:45:25 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('button').onclick = function () {
+  document.getElementById('confirm').innerHTML = 'Ordered Receidved';
+  document.getElementById('button').style.display = 'none';
+};
